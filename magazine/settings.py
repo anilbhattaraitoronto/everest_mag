4cbf975e4f905adcb1710893ba041b86463946fc
Anil Bhattarai <anilbhattarai@gmail.com>
2019-01-05 18:19:39 -0500

    fixed code on setting

--- magazine/settings.py
+++ magazine/settings.py
@@ -20,12 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'poz6cyx$iw%^z+da4a3!9#(6)!xd+8*u)(gl&x^=)f$#*j=n^%'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -150,7 +145,10 @@ CKEDITOR_CONFIGS = {
 
   }
 
-  try:
-      from .local_settings import *
-  except ImportError:
-      pass  
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+    
+
