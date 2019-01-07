3c71e38eac6074ab583faa16e8155bbc32d93283
Anil Bhattarai <anilbhattarai@gmail.com>
2019-01-06 22:32:33 -0500

    changed database to postgresql in the local dev server

--- magazine/settings.py
+++ magazine/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'magazine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'everest_prod',
+        'USER': 'anilbhattarai',
+        'PASSWORD':'anil@351617C',
+        'HOST' : 'localhost',
     }
 }
 
