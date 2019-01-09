1ea50c7e7b31c1c4a4a6f2abf0c500e6abacd56a
Anil Bhattarai <anilbhattarai@gmail.com>
2019-01-09 10:39:59 -0500

    added video slides

--- magazine/static/js/main.js
+++ magazine/static/js/main.js
@@ -1,14 +1,22 @@
 var slideIndex = 1;
+var videoIndex = 1;
 showSlides(slideIndex);
+showVideos(videoIndex);
 
 // Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
+function plusVideos(n) {
+    showVideos(videoIndex += n);
+}
 
 // Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
+}
+function currentVideo(n) {
+    showVideos(videoIndex = n);
 }
 
 function showSlides(n) {
@@ -25,4 +33,24 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+}
+
+function showVideos(n) {
+    var i;
+    var videos = document.getElementsByClassName('video-items');
+    var videoDots = document.getElementsByClassName('video-indicator');
+    if (n > videos.length) {
+        videoIndex = 1;
+    }
+    if (n < 1) {
+        videoIndex = videos.length;
+    }
+    for (i = 0; i < videos.length; i++) {
+        videos[i].style.display = 'none';
+    }
+    for (i = 0; i < videoDots.length; i++) {
+        videoDots[i].className = videoDots[i].className.replace(" active-video", "");
+    }
+    videos[videoIndex - 1].style.display = 'block';
+    videoDots[videoIndex - 1].className += " active-video";
 }
